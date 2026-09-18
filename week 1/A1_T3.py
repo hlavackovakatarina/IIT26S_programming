5a993b1f27c7677f0f675769a79d845fe683ef29
Katarína Hlavačková <hlavackova.katarina@gmail.com>
2026-09-18 15:01:12 +0300

    w1Task

---
+++ week 1/A1_T3.py
@@ -0,0 +1,3 @@
+Name=input("What is your name: ")
+print(f"Hi there {Name}")
+#print("Hi there"+input("what is your name: "))
---
+++ week 1/A1_T6.py
@@ -0,0 +1,5 @@
+Feed=int(input("Insert an integer: "))
+Value=Feed
+Remainder=Value%2
+print(f"Value is {Value}")
+print(f"The remainder is {Remainder} when {Value} is divided by 2.")
